a015f9f3ca60f7f4aba43567582f87d26c1d6513
flamboh <oliver.boorstein@gmail.com>
2025-05-09 11:43:59 -0700

    functionally loading data from one netflow capture file with nfdump

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,7 @@
-<script>
-	let text = '';
+<script lang="ts">
+	let today = new Date().toJSON().slice(0, 10);
+	let startDate = $state(today);
+	let text = $state([]);
 
 	async function loadText() {
 		const response = await fetch('/text');
@@ -11,7 +13,28 @@
 			console.error('Failed to load text');
 		}
 	}
+	function handleSubmit(e: Event) {
+		e.preventDefault(); // Prevent the default form submission
+		loadText(); // Call the loadText function when the form is submitted
+	}
 </script>
 
-<button on:click={loadText}>Load Text</button>
-<p class="text-2xl">{text}</p>
+<div class="flex h-screen w-screen flex-col justify-center bg-slate-600">
+	<form onsubmit={handleSubmit}>
+		<label>
+			<span class="hidden">Choose date:</span>
+			<input
+				class="m-1 w-48 bg-slate-300 hover:cursor-pointer"
+				type="date"
+				min={today}
+				bind:value={startDate}
+			/>
+		</label>
+		<label>
+			<span class="hidden">Choose time:</span>
+			<input class="m-1 w-48 bg-slate-300 hover:cursor-pointer" type="time" />
+		</label>
+	</form>
+	<button class="text-4xl text-white" onclick={loadText}>Load Data</button>
+	<p class="text-center text-2xl font-bold text-white">{text}</p>
+</div>
--- src/routes/text/+server.ts
+++ src/routes/text/+server.ts
@@ -6,11 +6,16 @@ const execPromise = promisify(exec);
 
 export async function GET() {
   try {
-    const { stdout } = await execPromise('cat ~/oliver/data'); // Execute the command and wait for the result
-    const result = `${stdout}`; // Trim the output to remove extra newlines
+
+    const { stdout } = await execPromise(
+      'nfdump -I -r ~/../../research/tango_cis/uonet-in/oh-ir1-gw/2025/05/08/nfcapd.202505081240'
+    );
+
+    // Process and return the result
+    const result = stdout.split(/[:\n]+/).filter((element, index) => index % 2 === 1);
     return json({ result }); // Return the result as JSON
   } catch (error) {
     console.error(`exec error: ${error}`);
-    return json({ error: 'Failed to retrieve working directory' }, { status: 500 });
+    
   }
 }
