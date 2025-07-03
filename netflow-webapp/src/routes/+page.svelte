ea918e3b8d760fa19a9a50ff4dd7fbb51d5a2f24
flamboh <oliver.boorstein@gmail.com>
2025-07-03 15:20:52 -0700

    changed month span to be the 31 days around the clicked point

--- netflow-webapp/src/routes/+page.svelte
+++ netflow-webapp/src/routes/+page.svelte
@@ -20,16 +20,16 @@
 
 	let dataOptions: { label: string; index: number; checked: boolean }[] = $state([
 		{ label: 'Flows', index: 0, checked: true },
-		{ label: 'Flows TCP', index: 1, checked: false },
-		{ label: 'Flows UDP', index: 2, checked: false },
-		{ label: 'Flows ICMP', index: 3, checked: false },
-		{ label: 'Flows Other', index: 4, checked: false },
-		{ label: 'Packets', index: 5, checked: true },
+		{ label: 'Flows TCP', index: 1, checked: true },
+		{ label: 'Flows UDP', index: 2, checked: true },
+		{ label: 'Flows ICMP', index: 3, checked: true },
+		{ label: 'Flows Other', index: 4, checked: true },
+		{ label: 'Packets', index: 5, checked: false },
 		{ label: 'Packets TCP', index: 6, checked: false },
 		{ label: 'Packets UDP', index: 7, checked: false },
 		{ label: 'Packets ICMP', index: 8, checked: false },
 		{ label: 'Packets Other', index: 9, checked: false },
-		{ label: 'Bytes', index: 10, checked: true },
+		{ label: 'Bytes', index: 10, checked: false },
 		{ label: 'Bytes TCP', index: 11, checked: false },
 		{ label: 'Bytes UDP', index: 12, checked: false },
 		{ label: 'Bytes ICMP', index: 13, checked: false },
@@ -74,9 +74,9 @@
 						if (groupBy === 'date') {
 							groupBy = 'hour';
 							const date = new Date(clickedElement.label);
-							// Set 1 month span around clicked date
-							const startOfMonth = new Date(date.getFullYear(), date.getMonth(), 1);
-							const endOfMonth = new Date(date.getFullYear(), date.getMonth() + 1, 0);
+							// Set 1 month span (30 days) centered around clicked date
+							const startOfMonth = new Date(date.getTime() - 15 * 24 * 60 * 60 * 1000);
+							const endOfMonth = new Date(date.getTime() + 16 * 24 * 60 * 60 * 1000);
 							startDate = startOfMonth.toISOString().slice(0, 10);
 							endDate = endOfMonth.toISOString().slice(0, 10);
 						} else if (groupBy === 'hour') {
