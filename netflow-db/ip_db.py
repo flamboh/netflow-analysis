38538db08860f0f6a009bb3aaeee6ad9898a424b
flamboh <oliver.boorstein@gmail.com>
2025-09-11 23:43:50 -0700

    parallelized processing of a day with hyperloglog

--- netflow-db/ip_db.py
+++ netflow-db/ip_db.py
@@ -3,6 +3,8 @@ import subprocess
 import os
 import sys
 from pathlib import Path
+import datetime as dt
+from multiprocessing import Pool
 
 # Load environment variables from .env file
 def load_env_file(env_path='../.env'):
@@ -46,22 +48,57 @@ def get_required_env(key):
 NETFLOW_DATA_PATH = get_required_env('NETFLOW_DATA_PATH')
 
 
+def process_file(current_time):
+  print(current_time)
+  hll = HyperLogLog()
+  file = f"{NETFLOW_DATA_PATH}/oh-ir1-gw/{current_time.strftime('%Y/%m/%d')}/nfcapd.{current_time.strftime('%Y%m%d%H%M')}"
+  if os.path.exists(file):
+    command = ["nfdump", "-r", file, "-q", "-o", "fmt:%sa", "-n", "0"]
+    result = subprocess.run(command, capture_output=True, text=True)
+    out = result.stdout.split("\n")
+    for line in out:
+      ip = line.strip()
+      hll.add(ip)
+    return hll, len(out) - 1
+  return hll, 0
+
 def main():
+  start_time = dt.datetime.now()
   hll = HyperLogLog()
 
-  file = "/research/tango_cis/uonet-in/oh-ir1-gw/2025/07/01/nfcapd.202507010000"
-  command = ["nfdump", "-r", file, "-q", "-o", "fmt:%sa", "-n", "0"]
-  
-  result = subprocess.run(command, capture_output=True, text=True)
+  first_file = dt.datetime(2025, 7, 1, 0, 0)
+  last_file = dt.datetime(2025, 7, 1, 23, 59)
+
   total = 0
-  for line in result.stdout.split("\n"):
-    ip = line.strip()
-    hll.add(ip)
-    total += 1
-  
+  all_files = [time for time in range(first_file, last_file, dt.timedelta(minutes=5))]
+  with Pool(processes=10) as pool:
+    results = pool.map(process_file, all_files)
+  for result in results:
+    total += result[1]
+    hll.merge(result[0])
+    
+
   print(hll.cardinality())
   print(total)
+  print(dt.datetime.now() - start_time)
   print(str(round(hll.cardinality() / total * 100, 2)) + "%")
 
 if __name__ == "__main__":
   main()
+
+
+#   file = "/research/tango_cis/uonet-in/oh-ir1-gw/2025/07/01/nfcapd.202507010000"
+#   command = ["nfdump", "-r", file, "-q", "-o", "fmt:%sa", "-n", "0"]
+  
+#   result = subprocess.run(command, capture_output=True, text=True)
+#   total = 0
+#   for line in result.stdout.split("\n"):
+#     ip = line.strip()
+#     hll.add(ip)
+#     total += 1
+  
+#   print(hll.cardinality())
+#   print(total)
+#   print(str(round(hll.cardinality() / total * 100, 2)) + "%")
+# if __name__ == "__main__":
+#   main()
